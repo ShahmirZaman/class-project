dba8a36ad83161d86ef5ebacc59abec54980f9fb
Shahmir Zaman <shahmirzaman009@gmail.com>
2023-12-11 01:32:25 +0500

    post edit functionality

--- Home/app.js
+++ Home/app.js
@@ -9,7 +9,10 @@ if(!loggedInUser) window.location.href = '../login/index.html'
 const postInput = document.querySelector("#postInput");
 const postImageUrl = document.querySelector("#imageUrl");
 let postDisplayArea = document.querySelector(".card-container");
+const submitBtn = document.getElementById('submitBtn');
 let imageUrl;
+let oldPost;
+let oldPostIndex;
 
 const postLocalStorage = JSON.parse(localStorage.getItem('posts')) || [];
 function postDisplayHandler() {
@@ -72,16 +75,63 @@ function logoutHandler() {
     window.location.href = '../login/index.html'
 }
 function deletePostHandler(deleteId) {
-  console.log("Delete Post",deleteId)
+  // console.log("Delete Post",deleteId)
   const forDeletePost = JSON.parse(localStorage.getItem('posts'))
   console.log(forDeletePost);
   const filteredPost = forDeletePost.filter((post) => {
      return post.id != deleteId;
   })
-  console.log(filteredPost);
+  // console.log(filteredPost);
   localStorage.setItem('posts',JSON.stringify(filteredPost))
   postDisplayHandler();
 }
 function editPostHandler(editId) {
   console.log("Edit Post",editId)
+  const postLocalStorage = JSON.parse(localStorage.getItem('posts'));
+  console.log("====>>>",postLocalStorage);
+  const findPost = postLocalStorage.find((post) => post.id == editId)
+  console.log("====>>>",findPost);
+  const findPostIndex = postLocalStorage.findIndex((post) => post.id === editId)
+  console.log("====>>>Post Index ",findPostIndex);
+
+  oldPost = findPost;
+  oldPostIndex = findPostIndex;
+
+  postInput.value = findPost.inputText;
+  submitBtn.innerHTML = "Update"
+  submitBtn.setAttribute("onclick","updatePostHandler()");
+}
+function updatePostHandler() {
+  let postObj;
+  console.log("ppost value",postInput.value)
+  if(imageUrl) {
+    console.log(imageUrl,"===>>>>imageUrl")
+    postObj = {
+      inputText:postInput.value,
+      image:imageUrl,
+      userDetails:JSON.parse(localStorage.getItem('loggedInUser')),
+    }
+  } else {
+    postObj = {
+      inputText:postInput.value,
+      userDetails:JSON.parse(localStorage.getItem('loggedInUser')),
+  }
+}
+console.log("PostObj+++++>>>",postObj);
+const updatePostData = {
+  id:oldPost?.id,
+  inputText:postObj.inputText || oldPost.inputText,
+  image:postObj.image || oldPost.image,
+  userDetails:JSON.parse(localStorage.getItem('loggedInUser')),
+}
+console.log("=>>>>>",updatePostData);
+const postLocalStorage = JSON.parse(localStorage.getItem('posts'));
+postLocalStorage.splice(oldPostIndex,1,updatePostData);
+console.log("....>>>>>",postLocalStorage);
+localStorage.setItem('posts',JSON.stringify(postLocalStorage));
+postDisplayHandler();
+submitBtn.innerHTML = "Submit";
+submitBtn.setAttribute("onclick","postInputHandler()");
+postInput.value = "";
+postImageUrl.value = "";
 }
